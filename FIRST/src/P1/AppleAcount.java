c86a6862d81c118ee92ff90a4f1f2c7f8b4e55a4
Abhishek <Abhishek@LAPTOP-DMSMHADL>
2021-06-09 15:31:23 +0530

    update3

--- FIRST/src/P1/AppleAcount.java
+++ FIRST/src/P1/AppleAcount.java
@@ -14,6 +14,7 @@ public class AppleAcount {
 	//public static void main(String[] args) {
 	{// TODO Auto-generated method stub
 	System.out.println("hi prends");
+	System.out.println("htfu");
 		ChromeDriver a =new ChromeDriver();
 		a.get("https://appleid.apple.com/account");
 		a.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
@@ -30,6 +31,7 @@ public class AppleAcount {
 		c.selectByVisibleText("+91 (India)");
 		a.findElementByXPath("(//input[@aria-required='true'])[7]").click();
 		a.findElementByXPath("(//input[@aria-required='true'])[7]").sendKeys("9515257489");
+		
 		
 		
 		
